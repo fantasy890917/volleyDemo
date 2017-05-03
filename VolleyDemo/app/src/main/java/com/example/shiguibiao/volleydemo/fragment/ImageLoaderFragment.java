a61924a76d7950d10b47e408862ce546ebc7e148
shiguibiao <shiguibiao@huaqin.com>
2017-05-03 17:12:43 +0800

    add demo src for learning

---
+++ VolleyDemo/app/src/main/java/com/example/shiguibiao/volleydemo/fragment/ImageLoaderFragment.java
@@ -0,0 +1,33 @@
+package com.example.shiguibiao.volleydemo.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.shiguibiao.volleydemo.R;
+import com.example.shiguibiao.volleydemo.adapter.ImageBaseAdapter;
+import com.example.shiguibiao.volleydemo.adapter.ImageLoaderAdapter;
+import com.example.shiguibiao.volleydemo.util.Constants;
+
+public class ImageLoaderFragment extends Fragment{
+	public static final int INDEX=22;
+	
+	private String[] imageUrlArray = Constants.IMAGE_URLS;
+
+	private GridView gvCar;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fr_image_request, container,false);
+
+		gvCar = (GridView) view.findViewById(R.id.gv_car);
+		ImageBaseAdapter adapter = new ImageLoaderAdapter(getActivity(),imageUrlArray);
+		gvCar.setAdapter(adapter);
+
+		return view;
+	}
+
+}
---
+++ VolleyDemo/app/src/main/java/com/example/shiguibiao/volleydemo/fragment/NetworkImageViewFragment.java
@@ -0,0 +1,32 @@
+package com.example.shiguibiao.volleydemo.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.shiguibiao.volleydemo.R;
+import com.example.shiguibiao.volleydemo.adapter.ImageBaseAdapter;
+import com.example.shiguibiao.volleydemo.adapter.NetworkImageViewAdapter;
+import com.example.shiguibiao.volleydemo.util.Constants;
+
+public class NetworkImageViewFragment extends Fragment{
+	public static final int INDEX=23;
+	
+	private String[] imageUrlArray = Constants.IMAGE_URLS;
+
+	private GridView gvCar;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fr_image_request, container,false);
+
+		gvCar = (GridView) view.findViewById(R.id.gv_car);
+		ImageBaseAdapter adapter = new NetworkImageViewAdapter(getActivity(),imageUrlArray);
+		gvCar.setAdapter(adapter);
+
+		return view;
+	}
+}
